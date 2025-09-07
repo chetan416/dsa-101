1d0fa2d8ad44ddefe18c2e2df50159e3fd53d1fa
chetan416 <bandaruchetan16@gmail.com>
2025-09-07 21:25:16 +0530

    Initial commit

---
+++ two_sum.py
@@ -0,0 +1,9 @@
+def twoSum(nums, target):
+    hashmap = {}
+    for i, num in enumerate(nums):
+        diff = target - num
+        if diff in hashmap:
+            return [hashmap[diff], i]
+        hashmap[num] = i
+
+print(twoSum([2, 7, 11, 15], 9))
