6565b70c6eb8f51acb4bd030e8c082b22b565ec8
chetan416 <bandaruchetan16@gmail.com>
2025-09-07 22:10:02 +0530

    Variables changes

--- valid_anagram.py
+++ valid_anagram.py
@@ -3,8 +3,8 @@ from collections import Counter
 def valid_anagram(first_string,second_string):
     if len(first_string) != len(second_string):
         return False
-    s_dict = Counter(first_string)
-    t_dict = Counter(second_string)
-    return s_dict == t_dict
+    first_string_dict = Counter(first_string)
+    second_string_dict = Counter(second_string)
+    return first_string_dict == second_string_dict
 
 print(valid_anagram("anagram","nagaram"))
