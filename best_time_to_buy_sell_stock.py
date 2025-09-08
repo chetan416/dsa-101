5fdb7a23914f33c1feaf3c87d180e11aaf9f9adf
chetan416 <bandaruchetan16@gmail.com>
2025-09-08 13:08:44 +0530

    changed -inf to inf

--- best_time_to_buy_sell_stock.py
+++ best_time_to_buy_sell_stock.py
@@ -1,5 +1,5 @@
 def buy_sell_stock(prices):
-    min_price = float("-inf")
+    min_price = float("inf")
     max_profit = 0
 
     for price in prices:
@@ -7,3 +7,5 @@ def buy_sell_stock(prices):
         potential_profit = price - min_price
         max_profit = max(max_profit,potential_profit)
     return max_profit
+
+print(buy_sell_stock([7,1,5,3,6,4]))
