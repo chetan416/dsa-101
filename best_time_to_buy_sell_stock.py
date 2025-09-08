9336b61ad5265639e89bd1c1f97fcbc1916a8254
chetan416 <bandaruchetan16@gmail.com>
2025-09-08 11:26:20 +0530

    best time to buy and sell stocks

---
+++ best_time_to_buy_sell_stock.py
@@ -0,0 +1,9 @@
+def buy_sell_stock(prices):
+    min_price = float("-inf")
+    max_profit = 0
+
+    for price in prices:
+        min_price = min(min_price,price)
+        potential_profit = price - min_price
+        max_profit = max(max_profit,potential_profit)
+    return max_profit
